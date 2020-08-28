6d078c86972cb34b5c06356937d4c5eed4243d50
jorge <jorgelllreynaldez@gmail.com>
2020-08-27 21:43:24 -0400

    Impuestos

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -7,6 +7,21 @@ import { Component } from '@angular/core';
 })
 export class Tab1Page {
 
+  Monto:number;
+  calcularIVA = 0;
+  calcularIT = 0;
+  calcularUtilidad = 0;
+  IVA = 0.13;
+  IT = 0.03;
+  
   constructor() {}
 
+  calcularImpuestos() {
+
+    this.calcularIVA = this.Monto * this.IVA;
+    this.calcularIT = this.Monto * this.IT;
+    this.calcularUtilidad = this.Monto - this.calcularIVA - this.calcularIT;
+  
+  }
+  
 }
